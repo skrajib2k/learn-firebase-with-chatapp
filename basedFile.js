0a44e221a2cb4e9778cd14ec98be759016ee6daf
Md Rajib Sheikh <98007729+skrajib2k@users.noreply.github.com>
2024-10-26 10:05:34 +0600

    upload

--- basedFile.js
+++ basedFile.js
@@ -1,1 +1,22 @@
-
+// ==> file uploadImage firestore return image uploaded url*
+async function upload(file){
+    const date=new Date()
+    const storageRef = ref(storage, `images/${date + 'rivers.jpg'}`);
+const uploadTask = uploadBytesResumable(storageRef, file);
+return new Promise((resolve,reject)=>{
+uploadTask.on('state_changed', 
+  (snapshot) => {
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+  }, 
+  (error) => {
+    reject('Something went worng!'+ error.code)
+  }, 
+  () => {
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        resolve(downloadURL)
+    });
+})
+  }
+);
+}
